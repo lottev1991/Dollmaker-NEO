dc54986362722481342a1188f2d61c45709a2167
Lotte V <61566058+lottev1991@users.noreply.github.com>
2025-01-12 22:48:37 +0100

    Fixes from master

--- scripts/drag.js
+++ scripts/drag.js
@@ -18,14 +18,8 @@ function toggleFullScreen() { /* Add full screen toggle. Mostly did this for mob
 function prepareDoll() {
 	$("#bodyArea").css({
 		"background-color": "transparent",
+		"border": "1px solid transparent", /* Make border transparent so it won't show in the final image */
 		"border-radius": "0px", /* Resets border radius so the rounded corners are not visible on the final image */
-
-		/** This is necessary on mobile apparently, to move the border out of sight. */
-		"width": "250px",
-		"height": "401px",
-	});
-	$(".ui-tabs-hide").css({
-		"visibility": "visible",
 	});
 }
 
@@ -33,12 +27,8 @@ function prepareDoll() {
 function revertDoll() {
 	$("#bodyArea").css({
 		"background-color": "",
+		"border": "",
 		"border-radius": "",
-		"width": "",
-		"height": "",
-	});
-	$(".ui-tabs-hide").css({
-		"visibility": "",
 	});
 }
 $(document).ready(function () {
@@ -125,6 +115,7 @@ $(document).ready(function () {
 			x: 1,
 			y: 1,
 			scale: 1, /* Renders the final canvas at a zoom level of 1 at all times, otherwise your final image will be zoomed in along with the page and we don't want that.  */
+			imageSmoothingEnabled: false, /* This is a custom setting that I added to my fork of html2canvas. When set to "false", it makes sure the final image remains pixelated no matter the zoom level and/or scaling ("true" is the default and does the opposite). NOTE: I've coded it so that setting "image-rendering: pixelated" and "image-rendering: crisp-edges" in CSS stylesheets will accomplish the same thing.*/
 		}).then(canvas => {
 			canvas.toBlob(function (blob) {
 				/* Revert the CSS of the body div and tabs back to normal. */
@@ -156,6 +147,7 @@ $(document).ready(function () {
 			x: 75,
 			y: 36,
 			scale: 1,
+			imageSmoothingEnabled: false,
 		}).then(canvas => {
 			canvas.toBlob(function (blob) {
 				revertDoll();
